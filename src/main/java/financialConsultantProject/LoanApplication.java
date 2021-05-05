d722fc34749e12b0d268792bea1a160842a4fd00
Jeyalakshmi S <jeya2671@global.chainsys.com>
2021-05-05 14:51:37 +0530

    pom.xml file and src file

---
+++ src/main/java/financialConsultantProject/LoanApplication.java
@@ -0,0 +1,39 @@
+package financialConsultantProject;
+
+import java.util.*;
+
+public class LoanApplication {
+
+	static ArrayList<loginPage> detailLists=new ArrayList<loginPage>();
+	
+	 
+	 static {
+		 
+	 loginPage login4=new loginPage();
+	 login4.name="Jeya";
+	 login4.fatherName="SivaKumar";
+	 login4.age=21;
+	 login4.gender="Female";
+	 login4.email="jeya04632@gmail.com";
+	 login4.address="4/230,Mettu Street, Nalattinputhur, Kovilpatti";
+	 login4.mobileNo=7305166720L;
+	 login4.adharNo=987623451202L;
+	 detailLists.add(login4);
+	 
+	 }
+	 	 
+	public static boolean ValidDiplayApplicationForm(String name,String FatherName,int age,String gender,String email,long mobileno,long adharno,String address) {
+	
+	boolean isExists=false;
+
+	for(loginPage lists:detailLists) {
+	if(lists.name.equalsIgnoreCase(name) && lists.fatherName.equalsIgnoreCase(FatherName)&&lists.age==age&&lists.gender.equalsIgnoreCase(gender)&&lists.email.equalsIgnoreCase(email)&&lists.mobileNo==mobileno&&lists.adharNo==adharno&&lists.address.equalsIgnoreCase(address)) {
+	isExists=true;
+	}
+	
+	}
+	return isExists;
+	
+	}
+
+}
